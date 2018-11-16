0d8c73d80f40bd782c5a7885642b10d88032ec30
bibek <bibeksinghthapa@gmail.com>
2018-11-16 17:01:14 -0500

    imageList done

--- src/components/App.js
+++ src/components/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import unsplash from '../api/unsplash';
 import SearchBar from './SearchBar';
+import ImageList from './ImageList'
 
 class App extends React.Component{
   state = {images:[]};
@@ -10,6 +11,7 @@ class App extends React.Component{
     const response = await unsplash.get('search/photos',{
         params:{query:term}
       });
+
       this.setState({images: response.data.results});
 
   }
@@ -20,7 +22,7 @@ class App extends React.Component{
     return (
       <div className='ui container' style={{marginTop: '15px'}}>
         <SearchBar onSubmit={this.onSearchSubmit}/>
-        <p>{this.state.images.length}</p>
+        <ImageList images ={this.state.images}/>
       </div>
     )
   }
